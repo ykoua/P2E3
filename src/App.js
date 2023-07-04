1d0845145d35cb56a170f12bf6edf96aeabef5e4
ykoua <64792587+ykoua@users.noreply.github.com>
2023-07-04 01:17:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavComponent from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Services from './Services';
+import Contact from './Contact';
+import Booking from './Booking';
+
 
 function App() {
   return (
+    
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavComponent />
+      <div className='content'>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>  
+          <Route path='/Services'>
+            <Services />
+          </Route>   
+          <Route path='/Contact'>
+            <Contact />
+          </Route>  
+          <Route path='/Booking'>
+            <Booking />
+          </Route> 
+      </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
